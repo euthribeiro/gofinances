05e1daeff6d4bcce263a0363144f183f89c3230b
Thiago Ribeiro <thiago.santos@selfpay.com.br>
2021-05-30 00:51:44 -0300

    header and card transactions

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { HighlightCard } from '../../components/HighlightCard';
+import {
+  Container,
+  Header,
+  UserInfo,
+  Photo,
+  User,
+  UserGretting,
+  UserName,
+  UserWrapper,
+  Icon,
+  HighlightCards
+} from './styles';
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{uri: 'https://avatars.githubusercontent.com/u/36652917?s=400&u=162ebd4607d72e5b84b4f699d07ca3f1517ab7e2&v=4.png'}} />
+            <User>
+              <UserGretting>Olá,</UserGretting>
+              <UserName>Thiago</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+      <HighlightCards>
+        <HighlightCard 
+          title="Entradas" 
+          amount="R$ 17.400,00" 
+          lastTransaction="Última entrada dia 13 de abril"
+          type="up"
+        />
+        <HighlightCard 
+          title="Saídas"
+          amount="R$ 10.000,00"
+          lastTransaction="Última saída dia 13 de abril"
+          type="down"
+        />
+        <HighlightCard 
+          title="Total"
+          amount="R$ 7.400,00"
+          lastTransaction="01 à 16 de abril"
+          type="total"
+        />
+      </HighlightCards>
+    </Container>
+  )
+}
